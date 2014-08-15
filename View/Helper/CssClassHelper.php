2c4a226743172ef09ca818a498e89a44d0bf0582
David Yell <dyell@ukwebmedia.com>
2014-08-15 12:16:56 +0100

    Revert "First attempt at dealing with accented characters"
    This reverts commit 02b7c2efce1c71cfa077176975a8c32d16b6c42d.

--- View/Helper/CssClassHelper.php
+++ View/Helper/CssClassHelper.php
@@ -11,13 +11,6 @@ App::import('Vendor', 'CssSafeSlug.Numbertext', array('file' => 'number-text-sor
 class CssClassHelper extends AppHelper {
 	
 	public function convert($slug) {
-
-		// Strip out any accented characters and transliterate them into a regular character
-		setlocale(LC_ALL, 'en_GB');
-		$slug = iconv("UTF-8", "ASCII//TRANSLIT", $slug);
-		$slug = str_replace("'", "", $slug);
-		$slug = str_replace('"', "", $slug);
-
 		if (preg_match("/^[0-9]+/", $slug, $matches)) {
 			$n2w = new Numbertext();
 			$converted = $n2w->numbertext($matches[0], 'en_US');
